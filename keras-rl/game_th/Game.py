13e90fba5f183028f88d942e0f6d88b479bd2c23
0x384c0 <0x384c0@gmail.com>
2018-09-26 10:31:16 +0600

    added emitters sets

--- keras-rl/game_th/Game.py
+++ keras-rl/game_th/Game.py
@@ -3,6 +3,7 @@ import sys
 import curses
 import traceback
 import copy
+import random
 from collections import namedtuple
 
 from helpers.utils import *
@@ -43,13 +44,26 @@ class Game():
 	def __init__(self):
 		self._myscreen = curses.initscr() if RENDER else None
 
+		origin = Point(FIELD_SIZE.w/2, FIELD_SIZE.h * 0.8)
+		self._emitters_sets = [[
+			CircleWithHoleBulletEmitter	(origin=origin, delay=12, speed=0.5, num_rays=80, angle_min=PI * -0.80, angle_max=PI * 0.80, angle_generator=AngleGeneratorSine(diff=PI*0.25, period=80)),
+		],[
+			BulletEmitter				(origin=origin, delay=1,  speed=1, angle=PI * 0., angle_generator=AngleGeneratorLinear(diff=PI * 1.2, period=30, start_offset=True)),
+			BulletEmitter				(origin=origin, delay=1,  speed=1, angle=PI * 2.,  angle_generator=AngleGeneratorLinear(diff=PI * -1.2, period=30, start_offset=False)),
+		],[
+			BulletEmitter				(origin=origin, delay=1,  speed=1, angle=PI, angle_generator=AngleGeneratorSine(diff=PI*0.25, period=30))
+		],[
+			CircleBulletEmitter			(origin=origin, delay=10, speed=1, num_rays=20),
+		]]
+		self._emitters = None
+
 	def _update_game_state(self):
 		self._game_state = np.zeros(FIELD_SIZE.shape()) # empty
 		self._game_state[self._player_position.x][self._player_position.y] = vocab[sym_player]
 		self._animation_time += 1
 
 		# emit bullets
-		for emitter in self._emitters:
+		for emitter in self._get_emitters():
 			bullet = emitter.emit(self._animation_time,self._bullets)
 
 		# move or delete bullets
@@ -65,7 +79,11 @@ class Game():
 	def __del__(self):
 		self.stop()
 
-
+	def _get_emitters(self):
+		EMITTER_RESET_TIME = 60
+		if self._animation_time % EMITTER_RESET_TIME == 0 or self._emitters == None:
+			self._emitters = random.choice(self._emitters_sets)
+		return self._emitters
 
 	# public
 	def print_controls(self):
@@ -73,15 +91,7 @@ class Game():
 			self._myscreen.addstr("q - quit, left_key - {}, right_key - {}, up_key - {}, down_key - {}".format(left_key,right_key,up_key,down_key))
 
 	def reset(self):
-		origin = Point(FIELD_SIZE.w/2, FIELD_SIZE.h * 0.8)
 		self._animation_time = 0
-		self._emitters = [
-		CircleWithHoleBulletEmitter	(origin=origin, delay=12, speed=0.5, num_rays=80, angle_min=PI * -0.80, angle_max=PI * 0.80, angle_generator=AngleGeneratorSine(diff=PI*0.25, period=80)),
-		# BulletEmitter				(origin=origin, delay=1,  speed=1, angle=PI * 0., angle_generator=AngleGeneratorLinear(diff=PI * 1.2, period=30, start_offset=True)),
-		# BulletEmitter				(origin=origin, delay=1,  speed=1, angle=PI * 2.,  angle_generator=AngleGeneratorLinear(diff=PI * -1.2, period=30, start_offset=False)),
-		# BulletEmitter				(origin=origin, delay=1,  speed=1, angle=PI, angle_generator=AngleGeneratorSine(diff=PI*0.25, period=30)),
-		# CircleBulletEmitter			(origin=origin, delay=10, speed=1, num_rays=20),
-		]
 		self._bullets = []
 		self._player_position = copy.copy(START_PLAYER_POSITION)
 		self._update_game_state()
